a3ab6a734118a77bbf65cda7ee308147860f97b2
yamolean <moririnmanro9608@gmail.com>
2020-01-16 14:30:19 +0900

    firstCommit,pod install

---
+++ weatherAPP RxSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherAPP RxSwift
+//
+//  Created by 矢守叡 on 2020/01/16.
+//  Copyright © 2020 yamolean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
